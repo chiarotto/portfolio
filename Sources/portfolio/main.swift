f83a37c06189b6bccde2034892da31fe65050f53
Chiarotto Alessandro <chiarotto.alessandro@gmail.com>
2021-11-07 21:19:13 +0100

    Use system folder structure and delete json

--- Sources/portfolio/main.swift
+++ Sources/portfolio/main.swift
@@ -9,27 +9,43 @@ struct PortfolioGen: ParsableCommand {
     var sourceFolder: String
 
     mutating func run() throws {
-        print("Run Portfolio.")
+        print("Run Portfolio sourceFolder = \(sourceFolder)")
         let portfolioFileName = "portfolio.json"
         let contents = try contentdOfDirectory(name: sourceFolder)
-        contents.forEach {
-            print("\($0.absoluteString)")
-        }
-        let portfolioURL = contents.first { $0.absoluteString.contains(portfolioFileName)}
-        guard let url = portfolioURL else {
-            print("\(portfolioFileName) cannot found.")
+        
+        guard contents.filter { $0.isDirectory }.count == 1 else {
+            print("More folders are found, organize sequences with a single root folder.")
             return
         }
-        guard let data = try? Data(contentsOf: url) else {
-            print("\(url) not found...")
-            return
+        
+        let rootFolderURL = contents.first { $0.isDirectory }!
+        let contentsOfRoot = try contentdOfDirectory(url: rootFolderURL)
+        print("Contents of \(rootFolderURL):")
+        var sequences:[Sequence] = []
+        try contentsOfRoot.forEach { url in
+            let name = url.absoluteURL.lastPathComponent
+            if url.isDirectory {
+                let directoryContents = try contentdOfDirectory(url: url)
+                var urlImages: [String] = []
+                directoryContents.forEach { url in
+                    if url.isImage {
+                        let range = url.absoluteString.range(of: rootFolderURL.lastPathComponent)
+                        let urlImage = String(url.absoluteString.suffix(from: range!.lowerBound))
+                        urlImages.append(urlImage)
+                    }
+                }
+                let sequence = Sequence(
+                    title: "\(url.lastPathComponent.replacingOccurrences(of: "_", with: " "))",
+                    images: urlImages,
+                    short: ""
+                )
+                print("Add sequence \(sequence)")
+                sequences.append(sequence)
+            }
         }
-        let decoder = JSONDecoder()
-        guard let portfolio = try? decoder.decode(Portfolio.self, from: data) else {
-            print("Cannot decode \(url)")
-            return
-        }
-        print("\(portfolio)")
+        let portfolio = Portfolio(title: rootFolderURL.lastPathComponent, sequences: sequences)
+       
+        
 
         let sourceFolderURL = URL(fileURLWithPath: sourceFolder)
         let menuItems = portfolio.sequences.menuItems()
@@ -46,6 +62,8 @@ struct PortfolioGen: ParsableCommand {
         print("Portfolio generated.")
     }
 
+  
+    
     private func render(portfolio: Portfolio, sequence: Sequence, menuItems: [MenuItem]) throws -> String {
         let fs = FileSystemLoader(paths: [ Path("templates/") ])
         let environment = Environment(loader: fs)
@@ -65,6 +83,10 @@ struct PortfolioGen: ParsableCommand {
         return folder
     }
 
+    private mutating func contentdOfDirectory(url: URL) throws -> [URL] {
+        return try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
+    }
+    
     private mutating func contentdOfDirectory(name: String) throws -> [URL] {
         let folder = URL(fileURLWithPath: "./\(name)")
         return try FileManager.default.contentsOfDirectory(at: folder, includingPropertiesForKeys: nil)
@@ -72,3 +94,16 @@ struct PortfolioGen: ParsableCommand {
 }
 
 PortfolioGen.main()
+
+
+extension URL {
+    var isDirectory: Bool {
+       return (try? resourceValues(forKeys: [.isDirectoryKey]))?.isDirectory == true
+    }
+    
+    var isImage: Bool {
+        lastPathComponent.hasSuffix(".jpg") ||
+        lastPathComponent.hasSuffix(".png") ||
+        lastPathComponent.hasSuffix(".jpeg")
+    }
+}
